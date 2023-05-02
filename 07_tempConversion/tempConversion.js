20148cd3354ad4c81d5accb63393e302b883eb5a
asonato <116381282+asonato@users.noreply.github.com>
2023-05-02 19:17:52 -0400

    Completed exercise 7 tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  if (isNaN(f)) {
+    return "ERROR";
+  }
+  return Math.round((f - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  if (isNaN(c)) {
+    return "ERROR";
+  }
+  return Math.round(((c * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
