6b4f226edc272f19c034893fdd27728c88520fc9
asonato <116381282+asonato@users.noreply.github.com>
2023-05-02 18:22:11 -0400

    Complete exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let sum = 0;
+  let first = Math.min(num1, num2);
+  let second = Math.max(num1, num2);
+  for (let i = first; i <= second; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
