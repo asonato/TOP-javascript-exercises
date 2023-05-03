f1acaeb5743f8ea21c60f46011aa8b004230016e
asonato <116381282+asonato@users.noreply.github.com>
2023-05-02 21:41:05 -0400

    Complete exercise 9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const string = str.toLowerCase().replace(/[^a-z]/g, "");
+  return [...string].reverse().join("") === string;
 };
 
 // Do not edit below this line
